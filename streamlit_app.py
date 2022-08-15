d4dbc843dcd72a3811651920976c46220d3384e7
Josephine <111113359+JosephineLeo@users.noreply.github.com>
2022-08-15 20:22:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.text('🥗Kale, spinach & Rocket smoothie')
 streamlit.text('🍳Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭Build your own Fruit smoothie 🥝🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
